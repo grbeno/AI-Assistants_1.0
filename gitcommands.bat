9469844cc38ffe2a541aa32a0e1bb0a32692e464
grbeno <grbeno@gmail.com>
2024-04-18 14:11:56 +0200

    update-repo

--- gitcommands.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set /p commitMessage=""
-git status
-git add .
-git commit -m "%commitMessage%"
-git push -u origin main
--- run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start powershell -Command "npm run build; pipenv run python manage.py runserver"
-explorer "http://localhost:8000/"
