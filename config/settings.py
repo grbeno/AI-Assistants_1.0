f0f24839480b0569b563e4e01c0c5edbfd3d793c
grbeno <grbeno@gmail.com>
2025-02-04 15:26:07 +0100

    try-static-1

--- config/settings.py
+++ config/settings.py
@@ -153,7 +153,7 @@ STATIC_URL = 'static/'
 if DEBUG:
     STATICFILES_DIRS = [ str(BASE_DIR.joinpath('build', 'static'))  ]
 else:
-    STATIC_ROOT = str(BASE_DIR.joinpath('build', 'static'))
+    STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 
 
 # Default primary key field type
