36faf20dfecda5601fcab13e349f4b64528414a9
grbeno <grbeno@gmail.com>
2025-04-05 18:43:30 +0200

    token-counting-debug-3

--- chat/routing.py
+++ chat/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import views
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/$", views.ChatConsumer.as_asgi(), name="chat"),
+    re_path(r"ws/chat/*", views.ChatConsumer.as_asgi(), name="chat"),
 ]
